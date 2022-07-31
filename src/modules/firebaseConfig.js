a7a7c0d1d1879858c3928a841ad155a3d9cfeaa7
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-07-31 12:52:06 +0500

    Add firebase config

---
+++ src/modules/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9sZFsznMBz9cc914ozcElOJKMeCgtoZs",
+  authDomain: "todo-list-1bd3c.firebaseapp.com",
+  projectId: "todo-list-1bd3c",
+  storageBucket: "todo-list-1bd3c.appspot.com",
+  messagingSenderId: "48165501555",
+  appId: "1:48165501555:web:efe824cd391d02de8f8806"
+};
+
+export function getFirebaseConfig() {
+  return firebaseConfig;
+}
